b13b70dd3d48eb2d10a3ec830e50a15b9656fadd
Stan61rus <stan61rus@yandex.ru>
2025-02-17 23:48:06 +0300

    Move in direction of focal point

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,16 +4,18 @@ public class PlayerController : MonoBehaviour
 {
     public float speed = 5f;
     private Rigidbody playerRb;
+    private GameObject focalPoint;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         playerRb = GetComponent<Rigidbody>();
+        focalPoint = GameObject.Find("FocalPoint");
     }
 
     // Update is called once per frame
     void Update()
     {
         float inputForvard = Input.GetAxis("Vertical");
-        playerRb.AddForce(Vector3.forward * speed * inputForvard);
+        playerRb.AddForce(focalPoint.transform.forward * speed * inputForvard);
     }
 }
